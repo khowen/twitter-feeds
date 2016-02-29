37d4fd1cd8e92ea196641032c709d7872566749b
Katie Owen <katherinehowen@gmail.com>
2016-02-29 12:43:19 -0500

    Tweets from hard coded search in app.js are rendering on front end.

--- app.js
+++ app.js
@@ -4,22 +4,12 @@ var app     = express();
 var port    = process.env.PORT || 3000;
 var router  = express.Router();
 var server  = require('http').createServer(app);
-var Twit    = require('twit');
-
-var twitter = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
-// console.log(twitter);
-
-var stream = twitter.stream('statuses/filter', { track: 'javascript'});
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
 app.use(morgan('dev'));
+app.use('/static', express.static('public'));
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'Twitter Search' });
@@ -30,8 +20,26 @@ server.listen(port);
 console.log('Server started on', port);
 
 var io = require('socket.io')(server);
-io.on('connect', function(socket) {
-  stream.on('tweet', function(tweet) {
-    socket.emit('tweets', tweet);
+var Twit    = require('twit');
+var twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+console.log(twitter);
+
+var stream = twitter.stream('statuses/filter', { track: 'drake'});
+console.log('STREAM', stream);
+
+io.on('connect', function(socket) {//creates socket
+  stream.on('tweet', function(tweet) { //gets info from Twitter
+    var data = {};
+    data.name = tweet.user.name;
+    data.screen_name = tweet.user.screen_name;
+    data.text = tweet.text;
+    data.user_profile_image = tweet.user.profile_image_url;
+    socket.emit('tweets', data); //send tweets to front end
+    console.log(data);
   });
 });

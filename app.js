70d288d1e5a3d7b944b6ff5f1b56933fb8230a30
Katie Owen <katherinehowen@gmail.com>
2016-02-29 11:12:21 -0500

    Added views dir, index.jade, and connected socket.io in app.js.

---
+++ app.js
@@ -0,0 +1,20 @@
+var morgan  = require('morgan');
+var express = require('express');
+var app     = express();
+var port    = process.env.PORT || 3000;
+var router  = express.Router();
+var server  = require('http').createServer(app);
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.use(morgan('dev'));
+
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Twitter Search' });
+});
+
+app.user('/', router);
+server.listen(port);
+console.log('Server started on', port);
+var io = require('socket.io')(server);

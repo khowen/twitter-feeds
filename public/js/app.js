d0b8cce03c49607f06c917c868dc78a2ab486f6e
Katie Owen <katherinehowen@gmail.com>
2016-03-01 01:23:22 -0500

    Updated styling and added logic to catch a entry of a blank string.

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,32 @@
 (function() {
   var socket = io();
+  var error  = '';
 
   socket.on('tweets', function(tweet) {
-    console.log(tweet);
+    // console.log(tweet);
     var userImage = '<div class="profile-image"><img src="' + tweet.user_profile_image + '" /></div>';
     var userName = '<p class="user-name">' + tweet.name + '</p><p class="user-handle"> @' +tweet.screen_name + '</p>';
     var tweetText = '<p class="tweet-text">' + tweet.text + '</p>';
-    var html = '<div class="tweet-wrapper">' + userName + userImage + tweetText + '</div>';
+    var html = '<div class="tweet-wrapper">' +  userImage + '<span>' + userName + tweetText + '</span></div>';
     $('#tweet-container').prepend(html)
   });
 
   $('form').on('submit', function() {
     event.preventDefault();
     var search_term = $('input').val().toLowerCase();
+    $('.error').text('');
+
+    if(search_term === ' ') {
+      error = 'Please enter a word to begin searching!';
+      $('.error').text(error);
+    }
+
     search_term.split(' ');
     socket.emit('updatedTerm', search_term);
   });
 
   socket.on('updatedTerm', function(searchTerm) {
-    $('h1').text('Twitter Search for ' + searchTerm);
+    // $('h1').text('Twitter Search for ' + searchTerm);
     $('#user-search').val('');
   });
 }());
